0065315f23d9c327d5dfe8190d82441694e8e2ad
KC <weilihmen@gmail.com>
2018-10-20 21:14:37 +0800

    try

--- config/deploy.rb
+++ config/deploy.rb
@@ -41,3 +41,15 @@ append :linked_dirs, '.bundle', "log", "tmp/pids", "tmp/cache", "tmp/sockets", "
 # set :ssh_options, verify_host_key: :secure
 
 set :passenger_restart_with_touch, true
+
+set :bundle_roles, :all                                         # this is default
+set :bundle_servers, -> { release_roles(fetch(:bundle_roles)) } # this is default
+set :bundle_binstubs, -> { shared_path.join('bin') }            # default: nil
+set :bundle_gemfile, -> { release_path.join('MyGemfile') }      # default: nil
+set :bundle_path, -> { shared_path.join('bundle') }             # this is default. set it to nil for skipping the --path flag.
+set :bundle_without, %w{development test}.join(' ')             # this is default
+set :bundle_flags, '--deployment --quiet'                       # this is default
+set :bundle_env_variables, {}                                   # this is default
+set :bundle_clean_options, ""                                   # this is default. Use "--dry-run" if you just want to know what gems would be deleted, without actually deleting them
+set :bundle_jobs, 4 # default: nil, only available for Bundler >= 1.4
+set :bundle_binstubs, -> { shared_path.join('bin') }
